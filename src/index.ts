57de5d3cdf15df6b878af4fb29a17589848fc910
Binh Bui Van <bvbinh@sk-global.biz>
2021-04-28 16:00:53 +0700

    relase

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,13 @@ import 'reflect-metadata';
 
 import { container } from 'tsyringe';
 import { ImageUrl } from './image-url.service';
-import { ConfigService } from './config.service';
 
 export function createClient(
   endpoint: string,
   bucketName: string,
-  secret?: string
 ): ImageUrl {
   const clientContainer = container.createChildContainer();
   clientContainer.register('endpoint', { useValue: endpoint });
   clientContainer.register('bucketName', { useValue: bucketName });
-
-  if (secret) {
-    clientContainer.resolve(ConfigService).set('signedUrl.secret', secret);
-  }
-
   return clientContainer.resolve(ImageUrl);
 }
