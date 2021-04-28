748c4895a2102f9b447b5e22b1cf6b4839b9e18f
Binh Bui Van <bvbinh@sk-global.biz>
2021-04-28 16:00:08 +0700

    xxx

--- src/image-url.service.ts
+++ src/image-url.service.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { inject, injectable } from 'tsyringe';
 import { URL } from 'url';
-import { ConfigService } from './config.service';
 import { Signer } from './interfaces';
-import { UrlSigner } from './signed-url.service';
 import btoa from 'btoa';
 import { ResizeDto } from './resize.dto';
 
@@ -11,20 +9,17 @@ import { ResizeDto } from './resize.dto';
 export class ImageUrl {
   constructor(
     @inject('endpoint') private readonly endpoint: string,
-    @inject('bucketName') private readonly bucketName: string,
-    @inject(UrlSigner) private readonly urlSigner: Signer,
-    private readonly config: ConfigService
+    @inject('bucketName') private readonly bucketName: string
   ) {}
 
   private _buildUrl(
     imageId: string,
     dto: Partial<Omit<ResizeDto, 'url'>>
-  ): URL {
-    const url = new URL(this.endpoint);
+  ): string {
     const _edits: any = new ResizeDto(dto);
     if (_edits.toFormat && _edits.quality) {
       _edits[_edits.toFormat] = { quality: _edits.quality };
-      delete(_edits.quality)
+      delete _edits.quality;
     }
     // Set up the request body
     const request = {
@@ -33,17 +28,10 @@ export class ImageUrl {
       edits: _edits,
     };
 
-    console.log(request);
     // Setup encoded request
     const str = JSON.stringify(request);
     const enc = btoa(str);
-    url.pathname += `${enc}`;
-
-    if (this.config.get('signedUrl.secret')) {
-      this.urlSigner.sign(url);
-    }
-
-    return url;
+    return `${this.endpoint}/${enc}`;
   }
 
   url(imageId: string, dto: Partial<Omit<ResizeDto, 'url'>>): string {
